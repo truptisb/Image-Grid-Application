72b88184bf6222ca34bb882e3eacb0b262a3220f
truptisb <truptibaurashi29@gmail.com>
2025-04-21 11:17:43 +0530

    Update api_service.dart

--- api_service.dart
+++ api_service.dart
@@ -7,12 +7,14 @@ class ApiService {
   Future<List<String>> fetchImages() async {
     final url = Uri.parse('https://api.unsplash.com/photos?client_id=$apiKey');
 
-    try {
+   try {
       final response = await http.get(url);
 
       if (response.statusCode == 200) {
         final List<dynamic> data = json.decode(response.body);
-        return data.map((item) => item['urls']['small'] as String).toList();
+        
+        // Map each image data to ImageItem
+        return data.map((item) => ImageItem.fromJson(item)).toList();
       } else {
         throw Exception("Failed to load images");
       }
